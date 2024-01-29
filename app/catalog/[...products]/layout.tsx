f8d4d37bd436afa3e7b38e50742d38b374ea2dfb
Isteh <isteh.code@gmail.com>
2024-01-29 12:02:26 +0400

    modify some files

--- app/catalog/[...products]/layout.tsx
+++ app/catalog/[...products]/layout.tsx
@@ -1,7 +1,7 @@
 import { IProduct } from "@/src/interfaces/product.interface"
 
 export async function generateStaticParams() {
-    const products = await fetch('http://localhost:3000/products').then<IProduct[]>((response) =>
+    const products = await fetch('https://kmll64isusjhkp56.public.blob.vercel-storage.com/db-3qNUIysDnh0yEQ7WlPPzPMaOwHAShK.json').then<IProduct[]>((response) =>
         response.json())
     return products.map((product) => ({
         slug: `${product.type}/${product.id}`,
